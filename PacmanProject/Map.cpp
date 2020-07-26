bad96e52fbf5dbc7a5a88986b7cfaa786cd67b33
Juan Pablo Rodriguez <juanpablorn30@gmail.com>
2020-07-26 03:02:00 -0500

    Add support to more complex maps

--- PacmanProject/Map.cpp
+++ PacmanProject/Map.cpp
@@ -12,14 +12,19 @@ Map::~Map()
 
 void Map::loadMap(std::string path, int sizeX, int sizeY)
 {
-	char tile;
+	char c;
 	std::fstream mapFile;
 	mapFile.open(path);
 
+	int srcX, srcY;
+
 	for (int y = 0; y < sizeY; y++) {
 		for (int x = 0; x < sizeX; x++) {
-			mapFile.get(tile);
-			Game::addTile(atoi(&tile), x * 60, y * 60);
+			mapFile.get(c);
+			srcY = atoi(&c) * 32;
+			mapFile.get(c);
+			srcX = atoi(&c) * 32;
+			Game::addTile(srcX, srcY, x * 60, y * 60);
 			mapFile.ignore();
 		}
 	}
